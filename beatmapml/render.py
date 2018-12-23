0ab2b21b3eaf6e73d36290c4b816c974be1985c8
Shuhao <johnmave126@gmail.com>
2018-12-23 18:47:32 -0500

    Make snapshot of sliders

--- beatmapml/render.py
+++ beatmapml/render.py
@@ -1,5 +1,9 @@
 import numpy as np
+import cv2
+import math
 from typing import Tuple
+
+PropotionPosition = Tuple[float, float]
 
 __all__ = [
     'RenderContext'
@@ -16,11 +20,63 @@ class RenderContext:
             self._field_right,
             self._field_bottom) = field_box
 
-        self._lin_y, self._lin_x = np.ogrid[0:self._canvas_w, 0:self._canvas_h]
+        self._top_left = np.array([self._field_left, self._field_top])
+        self._bottom_right = np.array([self._field_right, self._field_bottom])
 
-    def create_mask(self, x, y, radius):
-        canvas_x = (1 - x) * self._field_left + x * self._field_right
-        canvas_y = (1 - y) * self._field_top + y * self._field_bottom
-        lin_x = self._lin_y - canvas_x
-        lin_y = self._lin_x - canvas_y
-        return lin_x * lin_x + lin_y * lin_y <= radius * radius
+    def create_note_mask(self,
+                         x: float,
+                         y: float,
+                         radius: float) -> np.ndarray:
+        """Create mask of a note
+
+            Args:
+                x (float): The x coordinate of center of the note.
+                y (float): The y coordinate of center of the note.
+                radius (float): The radius of the note.
+
+            Returns:
+                The mask of the note
+        """
+        pos = np.array([x, y])
+        canvas_pos = (1 - pos) * self._top_left + pos * self._bottom_right
+        canvas_pos = (canvas_pos * 8).astype(np.int16)
+        radius = int(radius * 8)
+        mask = np.zeros((self._canvas_w, self._canvas_h), dtype=np.uint8)
+        return cv2.circle(mask,
+                          tuple(np.flip(canvas_pos)),
+                          radius,
+                          color=1,
+                          thickness=-1,
+                          shift=3).astype(np.bool)
+
+    def create_strip_mask(self,
+                          start_pos: PropotionPosition,
+                          end_pos: PropotionPosition,
+                          width: float) -> np.ndarray:
+        """Create mask of a rectangular strip
+
+            Args:
+                start_pos (PropotionPosition): The (x, y) coordinate of start
+                    of the strip.
+                end_pos (PropotionPosition): The (x, y) coordinate of end of
+                    the strip.
+                width (float): The width of the strip.
+
+            Returns:
+                The mask of the strip
+        """
+        start_pos = np.array(start_pos)
+        end_pos = np.array(end_pos)
+        start_pos = (1 - start_pos) * self._top_left + \
+            start_pos * self._bottom_right
+        end_pos = (1 - end_pos) * self._top_left + end_pos * self._bottom_right
+        mask = np.zeros((self._canvas_w, self._canvas_h), dtype=np.uint8)
+        start_pos = (start_pos * 8).astype(np.int16)
+        end_pos = (end_pos * 8).astype(np.int16)
+        width = int(width)
+        return cv2.line(mask,
+                        tuple(np.flip(start_pos)),
+                        tuple(np.flip(end_pos)),
+                        color=1,
+                        thickness=width,
+                        shift=3).astype(np.bool)
--- beatmapml/snapshot.py
+++ beatmapml/snapshot.py
@@ -1,7 +1,8 @@
 from slider import Beatmap
 from slider.beatmap import Circle, Slider
 from slider.mod import ar_to_ms
-from typing import Tuple
+from slider.position import Point as TickPoint
+from typing import Tuple, List
 import numpy as np
 import math
 import itertools
@@ -59,80 +60,74 @@ class SnapshotProcessor():
         self._hitcircles = sorted(
             self._hitcircles, key=lambda circle: circle.time)
         for circle in self._hitcircles:
-            x_prop = circle.position[0] / MAX_PLAYFIELD[0]
-            y_prop = circle.position[1] / MAX_PLAYFIELD[1]
-            circle.mask = self._render_ctx.create_mask(
+            x_prop = circle.position.x / MAX_PLAYFIELD[0]
+            y_prop = circle.position.y / MAX_PLAYFIELD[1]
+            circle.mask = self._render_ctx.create_note_mask(
                 x_prop, y_prop, self._radius)
+            circle.time_ms = circle.time.total_seconds() * 1000
 
         self._sliders = sorted(
             self._sliders, key=lambda slider: slider.time)
+        strip_counter = itertools.count()
+        for slider in self._sliders:
+            slider.time_ms = slider.time.total_seconds() * 1000
+            slider.end_time_ms = slider.end_time.total_seconds() * 1000
+            slider.strips = self.make_strips(slider, strip_counter)
+
         last_hitcircle_time = (
-            self._hitcircles[-1].time.total_seconds() * 1000
+            self._hitcircles[-1].time_ms
             if len(self._hitcircles) > 0 else 0)
         last_slider_time = (
-            max((x.end_time.total_seconds() * 1000 for x in self._sliders))
+            max((x.end_time_ms for x in self._sliders))
             if len(self._sliders) > 0 else 0)
         self._end_time = max(last_hitcircle_time, last_slider_time)
 
     def make_snapshots(self) -> np.ndarray:
         tick = 0
+
         circle_start = 0
         circle_end = 0
+
         slider_start = 0
-        slider_counter = itertools.count()
-        slider_pool = []
-        num_slice = math.ceil(self._end_time / self._interval)
+        strip_pool = []
+
+        num_slice = math.floor(self._end_time / self._interval) + 2
+
         snapshot = np.zeros((num_slice,) + self._canvas_size + (2,))
+
         circle_count = np.zeros(self._canvas_size)
         circle_count_rc = np.zeros(self._canvas_size)
+
+        strip_count = np.zeros(self._canvas_size)
+        strip_count_rc = np.zeros(self._canvas_size)
         for snapshot_idx in range(num_slice):
-            while (circle_end > circle_start and
-                   self._hitcircles[circle_start].time.total_seconds() * 1000 <
-                   tick):
-                circle = self._hitcircles[circle_start]
-                circle_count[circle.mask] -= 1
-                circle_count_rc[circle.mask] = 1 / \
-                    (circle_count[circle.mask] + 1e-8)
-                circle_start += 1
-            while len(slider_pool) > 0 and slider_pool[0][0] < tick:
-                heapq.heappop(slider_pool)
+            circle_start, circle_end = self.update_circle_pool(
+                tick, circle_start, circle_end, circle_count, circle_count_rc)
 
-            while (circle_end < len(self._hitcircles) and
-                   self._hitcircles[circle_end].time.total_seconds() * 1000 <
-                   tick + self._lookahead):
-                circle = self._hitcircles[circle_end]
-                circle_count[circle.mask] += 1
-                circle_count_rc[circle.mask] = 1 / circle_count[circle.mask]
-                circle_end += 1
-            while (slider_start < len(self._sliders) and
-                   self._sliders[slider_start].time.total_seconds() * 1000 <
-                   tick + self._lookahead):
-                slider = self._sliders[slider_start]
-                heapq.heappush(
-                    slider_pool,
-                    (slider.time.total_seconds(),
-                     next(slider_counter),
-                     slider))
-                slider_start += 1
+            slider_start = self.update_slider_pool(
+                tick, slider_start, strip_pool, strip_count, strip_count_rc)
 
-            if len(slider_pool) == 0 and circle_end == circle_start:
+            if len(strip_pool) == 0 and circle_end == circle_start:
                 tick += self._interval
                 continue
 
             circle_canvas_raw = np.zeros(self._canvas_size)
             for i in range(circle_start, circle_end):
                 circle = self._hitcircles[i]
-                progress = (1 - (circle.time.total_seconds() * 1000 - tick) /
+                progress = (1 - (circle.time_ms - tick) /
                             self._lookahead)
                 circle_canvas_raw[circle.mask] += progress
             self.stamp_raw_canvas(snapshot[snapshot_idx, ..., 0],
                                   circle_canvas_raw, circle_count_rc)
-            """
-            slider_canvas_raw = np.zeros(self._canvas_size + (2,))
-            # TODO: add slider handling
+
+            slider_canvas_raw = np.zeros(self._canvas_size)
+            for (end_ms, _, mask, start_ms) in strip_pool:
+                progress = (1 - (end_ms - tick) /
+                            (end_ms - start_ms + self._lookahead))
+                slider_canvas_raw[mask] += progress
             self.stamp_raw_canvas(snapshot[snapshot_idx, ..., 1],
-                                  slider_canvas_raw[..., 0])
-            """
+                                  slider_canvas_raw, strip_count_rc)
+
             tick += self._interval
         return snapshot
 
@@ -175,3 +170,96 @@ class SnapshotProcessor():
                          raw_canvas: np.ndarray,
                          count_rc: np.ndarray) -> None:
         np.multiply(raw_canvas, count_rc, out=canvas)
+
+    def make_strips(self,
+                    slider: Slider,
+                    counter: itertools.count):
+        slider_start = slider.time_ms
+        slider_end = slider.end_time_ms
+        beats_per_ms = slider.num_beats / (slider_end - slider_start)
+        tick_interval = self._interval / 4
+        slider.tick_rate = beats_per_ms * tick_interval
+        tick_points = slider.tick_points
+        samples = [(p.x / MAX_PLAYFIELD[0],
+                    p.y / MAX_PLAYFIELD[1],
+                    p.offset.total_seconds() * 1000)
+                   for p in itertools.chain([TickPoint(slider.position.x,
+                                                       slider.position.y,
+                                                       slider.time)],
+                                            tick_points,
+                                            [tick_points[-1]])]
+        return [
+            self.make_strip(s[2], (s[0], s[1]), e[2],
+                            (e[0], e[1]), counter) + (slider_start,)
+            for (s, e) in zip(samples[:-1], samples[1:])
+        ]
+
+    def make_strip(self, start, start_pos, end, end_pos, counter):
+        time_ms = (start + end) / 2
+        mask = self._render_ctx.create_strip_mask(start_pos,
+                                                  end_pos,
+                                                  2 * self._radius)
+        return (time_ms, next(counter), mask)
+
+    def update_circle_pool(self,
+                           tick,
+                           start,
+                           end,
+                           circle_count,
+                           circle_count_rc):
+
+        count_mask = np.zeros(self._canvas_size, dtype=np.bool)
+        has_change = False
+
+        while (end < len(self._hitcircles) and
+               self._hitcircles[end].time_ms <
+               tick + self._lookahead):
+            circle = self._hitcircles[end]
+            circle_count[circle.mask] += 1
+            count_mask |= circle.mask
+            end += 1
+            has_change = True
+
+        while (end > start and
+               self._hitcircles[start].time_ms <
+               tick):
+            circle = self._hitcircles[start]
+            circle_count[circle.mask] -= 1
+            count_mask |= circle.mask
+            start += 1
+            has_change = True
+
+        if has_change:
+            circle_count_rc[count_mask] = 1 / (circle_count[count_mask] + 1e-8)
+
+        return start, end
+
+    def update_slider_pool(self,
+                           tick,
+                           start,
+                           strip_pool,
+                           strip_count,
+                           strip_count_rc):
+        count_mask = np.zeros(self._canvas_size, dtype=np.bool)
+        has_change = False
+        while (start < len(self._sliders) and
+               self._sliders[start].time_ms <
+               tick + self._lookahead):
+            slider = self._sliders[start]
+            for strip in slider.strips:
+                heapq.heappush(strip_pool, strip)
+                strip_count[strip[2]] += 1
+                count_mask |= strip[2]
+            start += 1
+            has_change = True
+
+        while len(strip_pool) > 0 and strip_pool[0][0] < tick:
+            (_, _, mask, _) = heapq.heappop(strip_pool)
+            strip_count[mask] -= 1
+            count_mask |= mask
+            has_change = True
+
+        if has_change:
+            strip_count_rc[count_mask] = 1 / (strip_count[count_mask] + 1e-8)
+
+        return start
